a1dc14badf438329602c119b905dc95ba1608b35
Derrick Ochieng <ochiengderrick424@gmal.com>
2018-04-26 14:25:21 +0300

    first commit

---
+++ app/src/main/java/com/example/ochieng_derrick/globsave/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.ochieng_derrick.globsave;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button signUp, signIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.LogInTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signIn = findViewById(R.id.sign_in);
+        signUp = findViewById(R.id.sign_up);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ToDo validation
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/ochieng_derrick/globsave/SignupActivity.java
@@ -0,0 +1,13 @@
+package com.example.ochieng_derrick.globsave;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
